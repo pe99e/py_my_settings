a0a773313c1544a9b8d7cbb23106e047bafab9a8
Giuseppe P <giuseppepatti882@gmail.com>
2024-10-22 15:59:42 +0100

    Update and rename settings.py to mysettings.py
    added versioning of ini file

--- Settings/mysettings/mysettings.py
+++ Settings/mysettings/mysettings.py
@@ -1,25 +1,25 @@
 from configparser import ConfigParser
 import os
 
-class MySettings():
-
-    def __init__(self,settings:dict)->None:
-        self.settings=settings
+SW_VERSION = 'v6'
 
 
-    def createinitfile(self)->None:
+class MySettings:
+
+    def __init__(self, settings: dict, sw_version: str) -> None:
+        self.settings = settings
+        self.config_file = 'config_'+sw_version+'.ini'
+
+    def create_init_file(self) -> None:
         if not os.path.isfile('config.ini'):
             self.load_and_save_settings('init')
 
+    def load_and_save_settings(self, mode: str) -> None:
 
-    def load_and_save_settings(self,mode:str)->None:
-
-        ''' the mode main is not necessary since I will change the value directly by opening the ini file'''
-
+        """ the mode main is not necessary since I will change the value directly by opening the ini file
+        """
         config = ConfigParser()
-        config.read('config.ini')
-
-
+        config.read(self.config_file)
 
         for key in self.settings:
 
@@ -41,30 +41,22 @@ class MySettings():
 
                 config.set('main', key, self.settings[key])  # minutes
 
-
-            with open('config.ini', 'w') as f:
+            with open(self.config_file, 'w') as f:
                 config.write(f)
-
-
-
-
-
-
 
     def load_all_settings(self)->list:
         config = ConfigParser()
-        config.read('config.ini')
+        config.read(self.config_file)
         #for each_section in config.sections():
       #  for (each_key, each_val) in config.items("main"):#each_section
         return [each_val for (each_key, each_val) in config.items("main")]
 
-
-    def load_specific_settings(self,*key_setting:str)->list:
+    def load_specific_settings(self, *key_setting: str) -> list:
         config = ConfigParser()
-        config.read('config.ini')
-        return [each_val for (each_key, each_val) in config.items("main")  if each_key in key_setting]
+        config.read(self.config_file)
+        return [each_val for (each_key, each_val) in config.items("main") if each_key in key_setting]
 
-    def load_setting(self,key_setting):
+    def load_setting(self, key_setting: str) -> str:
         config = ConfigParser()
-        config.read('config.ini')
+        config.read(self.config_file)
         return config.get('main', key_setting)
